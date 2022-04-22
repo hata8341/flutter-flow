d613647d2ea94d220f7c041353ca458ad3d5fcfe
hata <wmf8341@gmail.com>
2022-04-22 13:03:31 +0900

    key-widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import "dart:async";
-
 import 'package:flutter/material.dart';
-import 'package:practice/BussinessLogic.dart';
+import 'package:practice/statefulTile.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -23,42 +21,29 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, this.title}) : super(key: key);
   final String? title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  var intStream = StreamController<int>();
-  var stringStream = StreamController<String>.broadcast();
-  var generator = Generator();
-  var coodinator = Coordinator();
-  var consumer = Consumer();
-
-  void _incrementCounter() {
-    generator.generate();
-    setState(() {
-      _counter++;
-    });
-  }
-
+  late List<Widget> tiles;
   @override
   void initState() {
-    generator.init(intStream);
-    coodinator.init(intStream, stringStream);
-    consumer.init(stringStream);
-    coodinator.coorinate();
-    consumer.consume();
-
     super.initState();
+    //2つのStatefulWidgetが準備
+    tiles = [
+      StatefulTile(
+        key: UniqueKey(),
+      ),
+      StatefulTile(key: UniqueKey()),
+    ];
   }
 
-  @override
-  void dispose() {
-    intStream.close();
-    stringStream.close();
-    super.dispose();
+  // 入れ替え処理
+  void changeTiles() {
+    setState(() {
+      tiles.insert(1, tiles.removeAt(0));
+    });
   }
 
   @override
@@ -67,36 +52,12 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title!),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              key: const Key('counter'),
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            StreamBuilder<String>(
-                stream: stringStream.stream,
-                initialData: "",
-                builder: (context, snapshot) {
-                  return Text(
-                    'RANDOM : ${snapshot.data}',
-                    style: Theme.of(context).textTheme.headline4,
-                  );
-                })
-          ],
-        ),
-      ),
+      body: Row(children: tiles),
       floatingActionButton: FloatingActionButton(
-        key: const Key('increment'),
-        onPressed: _incrementCounter,
+        onPressed: changeTiles,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
